517fdd159df0db0dd513379532e3cea94fef81f6
离场悲剧 <wangboworks@126.com>
2016-11-22 18:12:42 +0800

    Add files via upload

--- azpr_soc/trunk/digital/rtl/global_config.h
+++ azpr_soc/trunk/digital/rtl/global_config.h
@@ -1,5 +1,5 @@
 `ifndef __GLOBAL_CONFIG_HEADER__
-    
+    `define __GLOBAL_CONFIG_HEADER__    
     `ifdef ACTIVE_HIGH
         `define POSITIVE_RESET
             `define RESET_EDGE      posedge
@@ -8,7 +8,6 @@
         `define POSITIVE_MEMORY
             `define MEM_ENABLE      1'b1
             `define MEM_DISABLE     1'b0
-        `
     `elsif ACTIVE_LOW
         `define NEGATIVE_RESET
             `define RESET_EDGE      negedge
@@ -17,13 +16,11 @@
         `define NEGATIVE_MEMORY
             `define MEM_ENABLE      1'b0
             `define MEM_DISABLE     1'b1
-            
     `endif
     //io select 
 
     `define IMPLEMENT_TIMER
     `define IMPLEMENT_UART
     `define IMPLEMENT_GPIO
-
 
 `endif
