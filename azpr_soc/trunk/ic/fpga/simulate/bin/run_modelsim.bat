efc3e5f3bcb6779195e4c021b097f85f68354f61
离场悲剧 <wangboworks@126.com>
2016-12-27 04:48:46 -0600

    Add files via upload
    1.更新UART class
    2.更新i2c class
    3.基本的uart传输正确，但是还要研究下虚接口

--- azpr_soc/trunk/ic/fpga/simulate/bin/run_modelsim.bat
+++ azpr_soc/trunk/ic/fpga/simulate/bin/run_modelsim.bat
@@ -1,6 +1,6 @@
 echo Using Modelsim in path
 vlib work
-vlog -timescale "1 ns / 1 ps" +incdir+../../../digital/rtl/header+../../../digital/rtl+../tb -f ../flist/rtl_sim.f
+vlog -timescale "1 ns / 1 ps" +incdir+../../../digital/rtl/header+../../../digital/rtl+../tb+../model -f ../flist/rtl_sim.f
 
 vsim -voptargs="+acc" -t 1ps -lib work work.Testbench
 
