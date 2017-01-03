1e1344c557cf7a03f040db2afb26c67f8cba0904
RogerWang <wangboworks@126.com>
2017-01-03 18:05:50 +0800

    修改格式

---
+++ azpr_soc/trunk/ic/digital/README.md
@@ -0,0 +1,11 @@
+# 说明
+本目录用于创建整个FPGA的验证环境，主要包括以下内容:
+
+- projects : FPGA工程主目录，包括FPGA工程的所有文件，如rtl、ucf、doc等，目前主要由ISE实现
+- simulate : FPGA工程仿真主目录- 
+- simulate/bin   : 仿真执行脚本目录
+  - simulate/tb    : 
+  - simulate/tc    :
+  - simulate/flist :
+  - simulate/model :
+- doc : 文档目录
--- azpr_soc/trunk/ic/digital/rtl/chip_top.v
+++ azpr_soc/trunk/ic/digital/rtl/chip_top.v
@@ -90,33 +90,33 @@ module chip_top (
     // 4.1 the clk generate module
     //------------------------------------------------------------------------------------------------    
     clk_gen clk_gen (
-        .clk_ref      (clk_ref  ),              
-        .reset_sw     (reset_sw ),             
-        .clk          (clk      ),                  
-        .clk_n        (clk_n    ),                 
-        .chip_reset   (chip_reset)            
+        .clk_ref        (clk_ref        ),              
+        .reset_sw       (reset_sw       ),             
+        .clk            (clk            ),                  
+        .clk_n          (clk_n          ),                 
+        .chip_reset     (chip_reset     )            
     );
     //------------------------------------------------------------------------------------------------
     // 4.2 the cpu chip
     //------------------------------------------------------------------------------------------------    
     chip chip (
-        .clk      (clk),                      
-        .clk_n    (clk_n),                     
-        .reset    (chip_reset)                
+        .clk            (clk            ),                      
+        .clk_n          (clk_n          ),                     
+        .reset          (chip_reset     )                
         /********** UART **********/
     `ifdef IMPLEMENT_UART
-        , .uart_rx  (uart_rx)                 
-        , .uart_tx  (uart_tx)                 
+        , .uart_rx      (uart_rx        )                 
+        , .uart_tx      (uart_tx        )                 
     `endif
     `ifdef IMPLEMENT_GPIO
     `ifdef GPIO_IN_CH  
-        , .gpio_in (gpio_in)                  
+        , .gpio_in      (gpio_in        )                  
     `endif
     `ifdef GPIO_OUT_CH 
-        , .gpio_out (gpio_out)                
+        , .gpio_out     (gpio_out       )                
     `endif
     `ifdef GPIO_IO_CH  
-        , .gpio_io  (gpio_io)                 
+        , .gpio_io      (gpio_io        )                 
     `endif
     `endif
     );
